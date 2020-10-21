c660e2759ba8cbab9a366f0292fb953e0e9752d0
jmcanterafonseca-iota <67736750+jmcanterafonseca-iota@users.noreply.github.com>
2020-10-21 08:58:10 +0200

    ensure PATH is properly set for docker-compose

--- bootstrap/bootstrap-amazonlinux.sh
+++ bootstrap/bootstrap-amazonlinux.sh
@@ -33,9 +33,12 @@ dockerInstall () {
   ## Start docker service
   sudo systemctl start docker
 
+  echo "Installing docker-compose ..."
   ## Install docker-compose
   sudo curl -L "https://github.com/docker/compose/releases/download/1.27.3/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
   sudo chmod +x /usr/local/bin/docker-compose
+  
+  export PATH=$PATH:/usr/local/bin
 }
 
 scriptsInstall () {
