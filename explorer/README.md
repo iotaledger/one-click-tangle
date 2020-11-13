dc14d08e56fff6d4135eefc5a289dfd35b04b763
Jose Manuel Cantera Fonseca <jmcanterafonseca@gmail.com>
2020-11-13 12:38:42 +0100

    First version of Tangle Exploter up and running

--- explorer/README.md
+++ explorer/README.md
@@ -7,16 +7,16 @@
 chmod +x tangle-explorer.sh
 ```
 
-* Start a new Tangle Explorer
+* Install and start a new Tangle Explorer
 
 *Warning: It destroys previous data.* 
 
 ```
-./tangle-explorer.sh start <network-definition.json>
+./tangle-explorer.sh install <network-definition.json>
 ```
 
 The `network-definition.json` parameter is optional. By default it will be taken the file present at
-`config/private-network.json`. The format of the file is described [here](https://github.com/iotaledger/explorer/blob/master/api/DEPLOYMENT.md).
+`config/private-network.json`. The format of the file is described [here](https://github.com/iotaledger/explorer/blob/master/api/DEPLOYMENT.md). A template for the file is [here](./config/private-network.json). 
 
 * Stop all the containers by running 
 ```
--- explorer/tangle-explorer.sh
+++ explorer/tangle-explorer.sh
@@ -4,22 +4,10 @@
 
 set -e
 
+### Initialization code
+
 EXPLORER_SRC=./explorer-src
-
-help () {
-  echo "usage: tangle-explorer.sh [start|stop] [json-file-with-network-details.json]"
-}
-
-clean () {
-  if [ -d $EXPLORER_SRC ]; then
-    rm -Rf $EXPLORER_SRC
-  fi
-}
-
-stopContainers () {
-  echo "Stopping containers..."
-	docker-compose --log-level ERROR down -v --remove-orphans
-}
+APP_DATA=./application-data
 
 if [ $#  -lt 1 ]; then
   echo "Illegal number of parameters"
@@ -32,7 +20,7 @@ network_file="$2"
 
 DEFAULT_NETWORK_FILE="config/private-network.json"
 
-if [ "$command" == "start" ]; then
+if [ "$command" == "install" ]; then
   if [ $# -lt 2 ]; then
     network_file="$DEFAULT_NETWORK_FILE"
   fi
@@ -44,40 +32,77 @@ if ! [ -f $network_file ]; then
   exit 1
 fi
 
-startExplorer () {
-  clean
+###################
 
-  git clone https://github.com/iotaledger/explorer $EXPLORER_SRC
+help () {
+  echo "usage: tangle-explorer.sh [install|start|stop] [json-file-with-network-details.json]"
+}
 
-  stopContainers
-
-  if ! [ -d ./application-data ]; then
-    mkdir ./application-data
+clean () {
+  if [ -d $EXPLORER_SRC ]; then
+    rm -Rf $EXPLORER_SRC
   fi
 
-  if ! [ -d ./application-data/network ]; then
-    mkdir ./application-data/network
+  if [ -d $APP_DATA ]; then
+    rm -Rf $APP_DATA
+  fi
+}
+
+stopContainers () {
+  echo "Stopping containers..."
+	docker-compose --log-level ERROR down -v --remove-orphans
+}
+
+
+prepareConfig () {
+  if ! [ -d $APP_DATA ]; then
+    mkdir $APP_DATA
+  fi
+
+  if ! [ -d $APP_DATA/network ]; then
+    mkdir $APP_DATA/network
   fi
 
   cp $network_file ./application-data/network/private-network.json
 
-  # Compile the Web App code. This will leave the compiled JS in the dist folder
-  docker-compose run --rm -w /usr/src/app webapp-compiler sh -c 'npm install --prefix=/package > /dev/null && npm run build 2> /dev/null'
+  # Configuration of the API Server
+  cp config/api.config.local.json $EXPLORER_SRC/api/src/data/config.local.json
 
+  # Configuration of the Web App
+  cp config/webapp.config.local.json $EXPLORER_SRC/client/src/assets/config/config.local.json
+
+  cp Dockerfile $EXPLORER_SRC/client/Dockerfile
+
+  rm $EXPLORER_SRC/client/package-lock.json
+}
+
+installExplorer () {
+  clean
+
+  git clone https://github.com/iotaledger/explorer $EXPLORER_SRC
+
+  # We stop container after having source code available otherwise docker-compose would fail
+  stopContainers
+
+  prepareConfig
+}
+
+startExplorer () {
   # Running the Explorer API
-  docker-compose --log-level ERROR up -d explorer-api
-
-  # Running the WebApp (NGINX using content generated)
-  docker-compose --log-level ERROR up -d explorer-webapp 
+  docker-compose --log-level ERROR up -d  --build
 }
 
 case "${command}" in
 	"help")
     help
     ;;
-	"start")
+	"install")
+    installExplorer
     startExplorer
     ;;
+  "start")
+		startExplorer
+		;;
   "stop")
 		stopExplorer
 		;;
