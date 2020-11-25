7a965c6768081ff32128c9805a4f85ee308847e2
Jose Manuel Cantera Fonseca <jmcanterafonseca@gmail.com>
2020-11-25 09:01:02 +0100

    Remove last references to old repo location

--- bootstrap/bootstrap.sh
+++ bootstrap/bootstrap.sh
@@ -10,8 +10,8 @@ if [ -f ./bootstrap-amazonlinux.sh ]; then
   rm -f ./bootstrap-amazonlinux.sh
 fi
 
-wget https://raw.githubusercontent.com/jmcanterafonseca-iota/IOTA-Tangle-Node-Deployment/master/bootstrap/parameters.sh
-wget https://raw.githubusercontent.com/jmcanterafonseca-iota/IOTA-Tangle-Node-Deployment/master/bootstrap/bootstrap-amazonlinux.sh
+wget https://raw.githubusercontent.com/iotaledger/tangle-deployment/master/bootstrap/parameters.sh
+wget https://raw.githubusercontent.com/iotaledger/tangle-deployment/master/bootstrap/bootstrap-amazonlinux.sh
 
 chmod +x ./parameters.sh
 chmod +x ./bootstrap-amazonlinux.sh
