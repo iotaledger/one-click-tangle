82b83746fa7670c5dbf37dbc16ac1e7507fe01c7
Jose Manuel Cantera <jose.cantera@iota.org>
2021-09-17 17:18:52 +0200

    Private Tangle with Autopeering

--- hornet-private-net/utils.sh
+++ hornet-private-net/utils.sh
@@ -4,12 +4,12 @@
 
 # Extracts the public key from a key pair
 getPublicKey () {
-  echo $(cat "$1" | tail -1 | cut -d ":" -f 2 | sed "s/ \+//g" | tr -d "\n" | tr -d "\r")
+  cat $1 | awk -F : '{if ($1 ~ /public key/) print $2}' | sed "s/ \+//g" | tr -d "\n" | tr -d "\r"
 }
 
 # Extracts the private key from a key pair
 getPrivateKey () {
-  echo $(cat "$1" | head -n 1 | cut -d ":" -f 2 | sed "s/ \+//g" | tr -d "\n" | tr -d "\r")
+  cat $1 | awk -F : '{if ($1 ~ /private key/) print $2}' | sed "s/ \+//g" | tr -d "\n" | tr -d "\r"
 }
 
 setCooPublicKey () {
@@ -18,23 +18,45 @@ setCooPublicKey () {
 }
 
 generateP2PIdentity () {
-  docker-compose run --rm "$1" hornet tool p2pidentity > $2
+  docker-compose run --rm "$1" hornet tool p2pidentity-gen > "$2"
 }
 
 setupIdentityPrivateKey () {
-  local private_key=$(cat $1 | head -n 1 | cut -d ":" -f 2 | sed "s/ \+//g" | tr -d "\n" | tr -d "\r")
+  local private_key=$(cat $1 | awk -F : '{if ($1 ~ /private key/) print $2}' | sed "s/ \+//g" | tr -d "\n" | tr -d "\r")
   # and then set it on the config.json file
-  sed -i 's/"identityPrivateKey": ".*"/"identityPrivateKey": "'$private_key'"/g' $2
+  sed -i 's/"identityPrivateKey": ".*"/"identityPrivateKey": "'$private_key'"/g' "$2"
 }
 
 # Extracts the peerID from the identity file
 getPeerID () {
-  local identity_file="$1"
-  local line_count=$(cat $identity_file | wc -l | tr -d ' ' | tr -d '\n' | tr -d '\r')
-  # determine the line number to fetch the peerID from, by default fetch it from the 3rd line
-  local line_number=3
-  if [ $line_count == "6" ]; then
-    line_number=4
-  fi
-  echo $(cat $identity_file | sed "$line_number"'q;d' | cut -d ":" -f 2 | sed "s/ \+//g" | tr -d "\n" | tr -d "\r")
+  cat $1 | awk -F : '{if ($1 ~ /PeerID/) print $2}' | sed "s/ \+//g" | tr -d "\n" | tr -d "\r"
+}
+
+getAutopeeringID () {
+   cat $1 | awk -F : '{if ($1 ~ /public key \(base58\)/) print $2}' | sed "s/ \+//g" | tr -d "\n" | tr -d "\r"
+}
+
+createSubfolders () {
+  local folders="$@"
+
+  for folder in $folders;  do
+    if ! [ -d "./$folder" ]; then
+      mkdir "./$folder"
+    fi
+  done
+}
+
+removeSubfolderContent () {
+  local folders="$@"
+
+  for folder in $folders; do
+    if [ -d "./$folder" ]; then
+      sudo rm -Rf ./"$folder"/*
+    fi
+  done
+}
+
+# Sets entry node for autopeering
+setEntryNode () {
+  sed -i 's/"entryNodes": \[.*\]/"entryNodes": \["'$1'"\]/g' "$2"
 }
