f42ed696c118d27034289bf29eadaa3d10dd1a7f
Jose Manuel Cantera <jose.cantera@iota.org>
2021-10-29 18:16:53 +0200

    One click Hornet on K8s

--- hornet-mainnet/hornet.sh
+++ hornet-mainnet/hornet.sh
@@ -8,6 +8,9 @@
 
 set -e
 
+chmod +x ./utils.sh
+source ./utils.sh
+
 help () {
   echo "usage: hornet.sh [install|update|start|stop] -p <peer_multiAdress> -i <docker_image>"
 }
@@ -45,8 +48,6 @@ if ! [ -x "$(command -v jq)" ]; then
     echo "You can install it following the instructions at https://stedolan.github.io/jq/download/"
     exit 156
 fi
-
-HORNET_UPSTREAM="https://raw.githubusercontent.com/gohornet/hornet/main/"
 
 #####
 
@@ -92,52 +93,9 @@ volumeSetup () {
         sudo chown -R 65532:65532 snapshots 
         sudo chown -R 65532:65532 p2pstore
     fi
-}
 
-# The coordinator public key ranges are obtained
-cooSetup () {
-    cat config/config-template.json | jq --argjson protocol \
-    "$(wget $HORNET_UPSTREAM/config.json -O - -q | jq '.protocol')" \
-    '. |= . + {$protocol}' > config/config.json
-}
-
-peerSetup () {
-    # We obtain a new P2P identity for the Node
-    set +e
-    docker-compose run --rm hornet tool p2pidentity-gen > p2pidentity.txt 2> /dev/null
-    # We try to keep backwards compatibility
-    if [ $? -eq 1 ]; then
-        docker-compose run --rm hornet tool p2pidentity > p2pidentity.txt
-         # Now we extract the private key (only needed on Hornet versions previous to 1.0.5)
-        private_key=$(cat p2pidentity.txt | head -n 1 | cut -d ":" -f 2 | sed "s/ \+//g" | tr -d "\n" | tr -d "\r")
-        # and then set it on the config.json file
-        sed -i 's/"identityPrivateKey": ".*"/"identityPrivateKey": "'$private_key'"/g' config/config.json
-    fi
-    set -e
-
-    # And now we configure our Node's peers
-    if [ -n "$peer" ]; then
-        echo "Peering with: $peer"
-        # This is the case where no previous peer definition was there
-        sed -i 's/\[\]/\[{"alias": "peer1","multiAddress": "'$peer'"}\]/g' config/peering.json
-        # This is the case for overwriting previous peer definition
-        sed -i 's/{"multiAddress":\s\+".\+"}/{"multiAddress": "'$peer'"}/g' config/peering.json
-    else
-        echo "Configuring autopeering ..."
-        autopeeringSetup
-    fi 
-}
-
-autopeeringSetup () {
-    # The autopeering plugin is enabled
-    cat config/config.json | jq '.node.enablePlugins[.node.enablePlugins | length] |= . + "Autopeering"' > config/config-autopeering.json
-
-    # Then the autopeering configuration is added from Hornet
-    cat config/config-autopeering.json | jq --argjson autopeering \
-    "$(wget $HORNET_UPSTREAM/config.json -O - -q | jq '.p2p.autopeering')" \
-    '.p2p |= . + {$autopeering}' > config/config.json
-
-    rm config/config-autopeering.json
+    # Template files are copied
+    cp config-template/*.json config
 }
 
 imageSetup () {
---
+++ hornet-mainnet/k8s/hornet-k8s.sh
@@ -0,0 +1,123 @@
+#!/bin/bash
+
+# Script to deploy a new Hornet Chrysalis Node on Kubernetes
+# hornet.sh deploy .- Deploys a new Hornet Node on the cluster
+# hornet.sh scale .- Scales Hornet
+# hornet.sh undeploy .- Undeploys the Hornet Node
+# hornet.sh update .- Updates the Hornet Node
+
+set -e
+
+help () {
+  echo "usage: hornet-k8s.sh [deploy|scale|update|undeploy]"
+  echo "Parameter: NAMESPACE=<Kubernetes Namespace>"
+  echo "Parameter: INSTANCES=<Number of Instances>"
+  echo "Parameter: IMAGE=<Docker Images to be used>"
+  echo "Parameter: PEER=<multiPeerAddress>"
+}
+
+##### Command line parameter processing
+
+command="$1"
+peer="$PEER"
+image="$IMAGE"
+namespace="$NAMESPACE"
+instances="$INSTANCES"
+
+if [ -z "$namespace" ]; then
+    namespace="tangle"
+fi
+
+if [ -z "$instances" ]; then
+    instances=1
+fi
+
+if [ $#  -lt 1 ]; then
+    echo "Illegal number of parameters"
+    help
+    exit 1
+fi
+
+if ! [ -x "$(command -v jq)" ]; then
+    echo "jq utility not installed"
+    echo "You can install it following the instructions at https://stedolan.github.io/jq/download/"
+    exit 156
+fi
+
+if ! [ -x "$(command -v kubectl)" ]; then
+    echo "kubectl utility not installed"
+    echo "You can install it following the instructions at https://kubernetes.io/docs/tasks/tools/"
+    exit 158
+fi
+
+#####
+
+cp ../config-template/*.json ./config
+chmod +x ../utils.sh
+source ../utils.sh
+
+imageSetup () {
+    # The image only is set if it is passed as parameter
+    # Otherwise the image is taken from the docker-compose
+    if [ -n "$image" ]; then
+        echo "Using image: $image"
+        sed -i 's/image: .\+/image: '$image'/g' docker-compose.yaml
+    fi
+
+    # We ensure we have the image before
+    docker-compose pull hornet
+}
+
+deployHornet () {
+    cooSetup
+
+    peerSetup
+
+    # Namespace on which the node or nodes will be living
+    kubectl create namespace $namespace --dry-run=client -o yaml | kubectl apply -f -
+
+    # Config Map is created or overewritten
+    kubectl -n $namespace create configmap hornet-config --from-file=config --dry-run=client -o yaml | kubectl apply -f -
+
+    # Service associated and Statefulset associated
+    kubectl apply -n $namespace -f hornet-rest-service.yaml
+    kubectl apply -n $namespace -f hornet-service.yaml
+    kubectl apply -n $namespace -f hornet.yaml
+}
+
+undeployHornet () {
+    kubectl delete -n $namespace -f hornet-rest-service.yaml
+    kubectl delete -n $namespace -f hornet-service.yaml
+    kubectl delete -n $namespace -f hornet.yaml
+    kubectl delete -n $namespace configmap hornet-config
+}
+
+scaleHornet () {
+    kubectl scale -n $namespace statefulsets hornet-set --replicas=$instances
+}
+
+######################
+## Script starts here
+######################
+case "${command}" in
+  "help")
+    help
+    ;;
+  "deploy")
+    deployHornet
+    ;;
+  "undeploy")
+    undeployHornet
+    ;;
+  "scale")
+    scaleHornet
+    ;;
+  "update")
+    updateHornet
+    ;;
+  *)
+	echo "Command not Found."
+	help
+	exit 127;
+	;;
+esac
