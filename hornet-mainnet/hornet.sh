10e36e0ac8d04b37d3fa59a79cba0e7244c1605a
Jose Manuel Cantera <jose.cantera@iota.org>
2022-01-14 14:43:40 +0100

    Fix peering setup

--- hornet-mainnet/hornet.sh
+++ hornet-mainnet/hornet.sh
@@ -89,26 +89,12 @@ cooSetup () {
 }
 
 peerSetup () {
-    # We obtain a new P2P identity for the Node
-    set +e
-    docker-compose run --rm hornet tool p2pidentity-gen > p2pidentity.txt 2> /dev/null
-    # We try to keep backwards compatibility
-    if [ $? -eq 1 ]; then
-        docker-compose run --rm hornet tool p2pidentity > p2pidentity.txt
-         # Now we extract the private key (only needed on Hornet versions previous to 1.0.5)
-        private_key=$(cat p2pidentity.txt | head -n 1 | cut -d ":" -f 2 | sed "s/ \+//g" | tr -d "\n" | tr -d "\r")
-        # and then set it on the config.json file
-        sed -i 's/"identityPrivateKey": ".*"/"identityPrivateKey": "'$private_key'"/g' config/config.json
-    fi
-    set -e
-
     # And now we configure our Node's peers
     if [ -n "$peer" ]; then
         echo "Peering with: $peer"
+        cp config/peering-template.json config/peering.json
         # This is the case where no previous peer definition was there
         sed -i 's/\[\]/\[{"alias": "peer1","multiAddress": "'$peer'"}\]/g' config/peering.json
-        # This is the case for overwriting previous peer definition
-        sed -i 's/{"multiAddress":\s\+".\+"}/{"multiAddress": "'$peer'"}/g' config/peering.json
     else
         echo "Configuring autopeering ..."
         autopeeringSetup
